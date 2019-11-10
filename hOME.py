2120eb9fadbda3e612ddf8c06b7162ff15b99de6
Sravanthi9963 <48953511+Sravanthi9963@users.noreply.github.com>
2019-11-10 12:02:37 +0530

    Add files via upload

--- hOME.py
+++ hOME.py
@@ -5,9 +5,13 @@ import random
 import time
 import sqlite3
 from tkinter import messagebox
+from tkinter import messagebox
+import pickle
+import perceptron
+from perceptron import Perceptron  
 r= Tk()
 #======================================
-r.title('Non Regular Registration Form')
+r.title('Home Page')
 r.geometry('1350x750+0+0')
 r.config(bg='brown')
 background_imag = PhotoImage(file="i2.png")
@@ -19,26 +23,26 @@ f1=Frame(r,width=800,height=700,bg='brown',relief=SUNKEN,padx=10)
 f1.pack(side=LEFT)
 f2=Frame(r,width=600,height=700,bg='brown',relief='ridge',padx=100)
 f2.pack(side=RIGHT)
-l=Label(r,font=('arial',30,'bold'),text='ELECTRIC GRID STABILITY',fg='black',bd=10,anchor='w')
+l=Label(r,font=('Times New Roman',30,'bold italic'),text='ELECTRIC GRID STABILITY',fg='black',bd=10,anchor='w')
 l.place(x=400,y=10)
 
 embed=Frame(r,width=600,height=400)
 embed.place(x=8,y=200)
-l1=Label(embed,font=('arial',18,'bold'),text='An electrical grid, electric grid or power grid,is',fg='black',bd=10,anchor='w')
+l1=Label(embed,font=('Times New Roman',18,'bold italic'),text='An electrical grid, electric grid or power grid,is',fg='black',bd=10,anchor='w')
 l1.place(x=50,y=10)
-l2=Label(embed,font=('arial',18,'bold'),text='an interconnected network for delivering electricity',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='an interconnected network for delivering electricity',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=50)
-l2=Label(embed,font=('arial',18,'bold'),text='Electricity from distributed generation constitutes',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='Electricity from distributed generation constitutes',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=90)
-l2=Label(embed,font=('arial',18,'bold'),text='only small amounts.',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='only small amounts.',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=130)
-l2=Label(embed,font=('arial',18,'bold'),text='..... Here we have developed some neural network ',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='..... Here we have developed some neural network ',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=170)
-l2=Label(embed,font=('arial',18,'bold'),text='training models on electric grid stability data  ',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='training models on electric grid stability data  ',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=210)
-l2=Label(embed,font=('arial',18,'bold'),text='Collected from UCI....... ',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='Collected from UCI....... ',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=250)
-l2=Label(embed,font=('arial',18,'bold'),text='For more information of Electric Grid Stability Click below...  ',fg='black',bd=10,anchor='w')
+l2=Label(embed,font=('Times New Roman',18,'bold italic'),text='For more information of Electric Grid Stability Click below...  ',fg='black',bd=10,anchor='w')
 l2.place(x=2,y=290)
 
 import webbrowser
@@ -48,7 +52,7 @@ url = "https://www.tandfonline.com/doi/abs/10.1080/14786450512331325910"
 def openweb():
     webbrowser.open(url,new=new)
 
-Btn = Button(embed, text = "Click here",width=15,font=('arial',15,'bold'),bg='brown',command=openweb)
+Btn = Button(embed, text = "Click here",width=15,font=('Times New Roman',15,'bold'),bg='brown',command=openweb)
 Btn.place(x=190,y=330)
 embed1=Frame(r,width=500,height=400)
 embed1.place(x=800,y=210)
@@ -56,6 +60,94 @@ embed1.place(x=800,y=210)
 background_imag1 = PhotoImage(file="i4.png")
 backgrounda1 = Label(embed1, image=background_imag1, bd=0)
 backgrounda1.pack()
+
+####################
+
+def user_form():
+    fw= Toplevel()
+    fw.title('User Data Form')
+    fw.geometry('1350x750+0+0')
+    fw.config(bg='brown')
+    background_imag = PhotoImage(file="form.png")
+    backgrounda = Label(fw, image=background_imag, bd=0)
+    backgrounda.pack()
+    embed=Frame(fw,width=600,height=400)
+    embed.place(x=8,y=200)
+    background_imag1 = PhotoImage(file="form1.png")
+    backgrounda1 = Label(embed, image=background_imag1, bd=0)
+    backgrounda1.pack()
+    rand=StringVar()
+    rand1=StringVar()
+    rand2=StringVar()
+    rand3=StringVar()
+    l=Label(fw,font=('Times New Roman',35,'bold italic'),text='User Data Form',fg='black',bd=10,anchor='w',bg="white")
+    l.place(x=500,y=20)
+    l=Label(fw,font=('Times New Roman',15,'bold italic'),text='Reaction time of participant(Tau): ',fg='black',bd=10,anchor='w',bg="white")
+    l.place(x=700,y=250)
+    t=Entry(fw,font=('Times New Roman',16),textvariable=rand,bd=10,insertwidth=4,bg='powder blue',justify='right',relief='flat')
+    t.place(x=1010,y=250)
+    l=Label(fw,font=('Times New Roman',15,'bold italic'),text='Nominal Power Consumed(p):         ',fg='black',bd=10,anchor='w',bg="white")
+    l.place(x=700,y=330)
+    t1=Entry(fw,font=('Times New Roman',16),textvariable=rand1,bd=10,insertwidth=4,bg='powder blue',justify='right',relief='flat')
+    t1.place(x=1010,y=330)
+    l=Label(fw,font=('Times New Roman',15,'bold italic'),text='coefficient(-->) to price elasticity   ',fg='black',bd=10,anchor='w',bg="white")
+    l.place(x=700,y=410)
+    t2=Entry(fw,font=('Times New Roman',16),textvariable=rand2,bd=10,insertwidth=4,bg='powder blue',justify='right',relief='flat')
+    t2.place(x=1010,y=410)
+    l=Label(fw,font=('Times New Roman',15,'bold italic'),text='Stability(Real,positive or negative):',fg='black',bd=10,anchor='w',bg="white")
+    l.place(x=700,y=490)
+    t3=Entry(fw,font=('Times New Roman',16),textvariable=rand3,bd=10,insertwidth=4,bg='powder blue',justify='right',relief='flat')
+    t3.place(x=1010,y=490)
+    def Function():
+
+        if __name__=='__main__':
+            with open('final.pkl', 'rb') as f:
+                mp= pickle.load(f)
+        x1=t.get()
+        x2=t1.get()
+        x3=t2.get()
+        x4=t3.get()
+        test=[]
+        for i in range(4):
+            test.append(float(x1.strip()))
+        for i in range(4):
+            test.append(float(x2.strip()))
+        for i in range(4):
+            test.append(float(x3.strip()))
+        test.append(float(x4.strip()))
+        print(test)
+        import numpy as np
+        y_test=mp.predict(np.array(test))
+        if y_test>0:
+            state='Unstable'
+        else:
+            state='Stable'
+        messagebox.showinfo('Result','The electric grid with your given data is %s electric Grid'%(state))
+        finw=Tk()
+        finw.title('User Data Form')
+        finw.geometry('1350x750+0+0')
+        finw.config(bg='aqua')
+        l=Label(finw,font=('Times New Roman',20,'bold italic'),text='The Electric Grid with your given inputs is ',fg='black',bd=10,anchor='w',bg='aqua')
+        l.place(x=200,y=200)
+        l=Label(finw,font=('Times New Roman',20,'bold italic'),text="Unstable",fg='black',bd=10,anchor='w',bg='aqua')
+        l.place(x=250,y=250)
+        l=Label(finw,font=('Times New Roman',20,'bold italic'),text="Grid Power Station",fg='black',bd=10,anchor='w',bg='aqua')
+        l.place(x=370,y=250)
+        l=Label(finw,font=('Times New Roman',20,'bold italic'),text="Thank You..........",fg='black',bd=10,anchor='w',bg='aqua')
+        l.place(x=360,y=330)
+        def fun1():
+            finw.destroy()
+        Btn = Button(finw, text = "Close",width=15,font=('arial',15,'bold'),bg='blue',relief='sunken',command=fun1)
+        Btn.place(x=340,y=400)
+    Btn = Button(fw, text = "Submit",width=15,font=('arial',15,'bold'),bg='powder blue',relief='sunken',command=Function)
+    Btn.place(x=900,y=580)
+    fw.mainloop()
+
+
+
+
+
+#################
 def Perceptron_Window():
     pw=Toplevel()
     pw.title("Perceptron")
@@ -96,6 +188,7 @@ def Perceptron_Window():
         l.place(x=300,y=30)
         l=Label(tw,font=('Times New Roman',30,'bold italic'),text="     with Perceptron Network      ",fg='black',bd=10,anchor='w',bg='DeepSkyBlue')
         l.place(x=300,y=90)
+        
         with open("PerceptronData.txt","r") as fp:
             data=fp.readlines()
         a=[]
@@ -107,30 +200,36 @@ def Perceptron_Window():
         f2.place(x=300,y=160)
         l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[0],fg='black',bd=10,anchor='w',bg='DeepSkyBlue')
         l.place(x=5,y=30)
-        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[1],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text="Size of Stable data values  : 3620",fg='black',bd=10,anchor='w',bg='DeepSkyblue')
         l.place(x=5,y=70)
-        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[5],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text="Size of Unstable data values: 6830 ",fg='black',bd=10,anchor='w',bg='DeepSkyblue')
         l.place(x=5,y=110)
-        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[6],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[5],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
         l.place(x=5,y=150)
-        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[7],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[6],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
         l.place(x=5,y=190)
-        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[8],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[7],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
         l.place(x=5,y=230)
-        btnl=Button(tw,text='Back', width=10,font=('arial',20,'bold'),bg='black',fg='white',relief='groove',command=trained)
-        btnl.place(x=150,y=510)
+        l=Label(f2,font=('Times New Roman',20,'bold italic'),text=a[8],fg='black',bd=10,anchor='w',bg='DeepSkyblue')
+        l.place(x=5,y=290)
+        l=Label(tw,font=('Times New Roman',20,'bold italic'),text=" Check the Results With your own inputs........ ",fg='black',bd=10,anchor='w',bg='DeepSkyBlue')
+        l.place(x=250,y=450)
+##########################
+        
+######################   
+        btnl=Button(tw,text='Check', width=10,font=('Times New Roman',20,'bold italic'),bg='black',fg='white',relief='groove',command=user_form)
+        btnl.place(x=180,y=530)
         def Code():
            import webbrowser
            new = 1
            url = "https://github.com/Lovely-Professional-University-CSE/major-project-ca-1-electrical_grid_stability-25-26-27-28/blob/master/%20training%20with%20perceptron.ipynb"
            webbrowser.open(url,new=new)
 
- 
-        btnl=Button(tw,text='Github Code', width=10,font=('arial',20,'bold'),bg='black',fg='white',relief='groove',command=Code)
-        btnl.place(x=750,y=510)
-        btnl=Button(tw,text='Training Models', width=15,font=('arial',20,'bold'),bg='black',fg='white',relief='groove',command=model_window)
-        btnl.place(x=400,y=510)
-    btnl=Button(f2,text='Start Training', width=15,font=('arial',20,'bold'),bg='black',fg='white',relief='raised',command=trained)
+        btnl=Button(tw,text='Github Code', width=10,font=('Times New Roman',20,'bold italic'),bg='black',fg='white',relief='groove',command=Code)
+        btnl.place(x=750,y=530)
+        btnl=Button(tw,text='Training Models', width=15,font=('Times New Roman',20,'bold italic'),bg='black',fg='white',relief='groove',command=model_window)
+        btnl.place(x=420,y=530)
+    btnl=Button(f2,text='Start Training', width=15,font=('Times New Roman',20,'bold italic'),bg='black',fg='white',relief='raised',command=trained)
     btnl.place(x=60,y=15)
     pw.mainloop()
 def model_window():
@@ -155,9 +254,9 @@ def model_window():
     mw.mainloop()
 embed2=Frame(r,width=500,height=100)
 embed2.place(x=800,y=500)
-l2=Label(embed2,font=('arial',18,'bold'),text='Click to Go to the data training Models',fg='black',bd=10,anchor='w')
+l2=Label(embed2,font=('Times New Roman',18,'bold italic'),text='Click to Go to the data training Models',fg='black',bd=10,anchor='w')
 l2.place(x=4,y=5)
-Btn = Button(embed2, text = "GO",width=15,font=('arial',15,'bold'),bg='orange',relief='ridge',command=model_window)
+Btn = Button(embed2, text = "GO",width=15,font=('Times New Roman',15,'bold italic'),bg='orange',relief='ridge',command=model_window)
 Btn.place(x=160,y=50)
 
 
