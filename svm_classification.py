2aaa1c46cf2fd1829088a4f795b7e4d22e503adf
Vishnubalu <38403733+Vishnubalu@users.noreply.github.com>
2019-11-11 14:47:04 +0530

    upadates done for pkl

--- svm_classification.py
+++ svm_classification.py
@@ -4,17 +4,21 @@ import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
-import seaborn as sns
-from sklearn.externals import joblib
+import seaborn as sn
+import pickle
 import glob
+import seaborn as sns
+import pickle
 
 data_set = glob.glob('*.csv')
+
 df = pd.read_csv(data_set[0])
+print(df.isnull().sum())
+
 print('COLUMNS : ', df.columns)
 print('shape of dataframe : ', df.shape)
 labels = df['stabf']
 df.drop(columns='stabf', inplace=True)
-#print('features sample : ', df.head())
 labels = labels.map({'unstable':0, 'stable':1})
 df['labels'] = labels
 corr = df.corr()
@@ -22,18 +26,23 @@ sns.heatmap(corr,
         xticklabels=corr.columns,
         yticklabels=corr.columns)
 
-#print('Labels sample : ', labels.head())
 x_train, x_test, y_train, y_test = train_test_split(df[df.columns[0:13]], labels, test_size=0.25, random_state=42)
+with open('lists.pkl', 'wb') as file:
+    pickle.dump([x_test, y_test], file)
 print('train_features : {}, train_labels : {}'.format(x_train.shape, y_train.shape))
 print('test_features : {}, test_labels : {}'.format(x_test.shape, y_test.shape))
 
 try:
-  saved_model = joblib.load('svm.pkl')
+    with open('svm.pkl', 'rb') as file:
+        saved_model = pickle.load(file)
 except:
-  clf = SVC(kernel = 'linear', C=75, gamma='auto')
-  clf.fit(x_train, y_train)
-  joblib.dump(clf, 'svm.pkl') 
-  saved_model = joblib.load('svm.pkl')
-  
+    clf = SVC(kernel = 'linear', C=75, gamma='auto')
+    clf.fit(x_train, y_train)
+    with open('svm.pkl', 'wb') as file:
+        pickle.dump(clf, file)
+    with open('svm.pkl', 'rb') as file:
+        saved_model = pickle.load(file)
+
+
 pred = saved_model.predict(x_test)
 print('accuracy : ', accuracy_score(pred, y_test))
